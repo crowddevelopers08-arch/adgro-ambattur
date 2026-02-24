09b1a0fd912312da3e6ea023d800ef331919f667
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-02-24 12:05:34 +0530

    cla

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,20 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${outfit.variable} antialiased`}>
       <head>
-     
+        {/* Microsoft Clarity Tracking Code */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "vm8yxsikje");
+            `,
+          }}
+        />
       </head>
       <body className="font-sans">
         {children}
