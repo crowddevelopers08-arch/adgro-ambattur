c5641f1875c30d24bda983b2f4257e89779116e8
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-03-07 18:58:15 +0530

    first commitsssssss

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,8 +77,33 @@ export default function RootLayout({
             `,
           }}
         />
+        
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KDRPNBRH');
+            `,
+          }}
+        />
       </head>
       <body className="font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KDRPNBRH"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        
         {children}
       </body>
     </html>
